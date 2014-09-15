153014f880c4ea54908ace616ca9810a99f18280
Erick Arenas <earen003@fiu.edu>
2014-09-14 21:29:56 -0400

    all deleted

--- protected/runtime/gii-1.1.13/ControllerCode.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array (
-  'template' => 'default',
-  'baseClass' => 'Controller',
-  'actions' => 'home createDocument shareDocument deleteDocument importDocument renameDocument exportDocument saveDocument openDocument listDocument getLocalUrl getRemoteUrl',
-);
