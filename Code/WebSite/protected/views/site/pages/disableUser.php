4002a2440f0da89cab936b6653eb141c5c48f315
Rene Alfonso <ralfo028@fiu.edu>
2015-06-12 00:57:34 -0400

    Disabled User Page

--- Code/WebSite/protected/views/site/pages/disableUser.php
+++ Code/WebSite/protected/views/site/pages/disableUser.php
@@ -1,10 +1,15 @@
 <?php
 /* @var $this SiteController */
+    
+    // Shows a message to the user telling them that the account has been banned.
 
 $this->pageTitle=Yii::app()->name . ' - About';
 $this->breadcrumbs=array(
 	'Verify Account',
 );
 ?>
-<h1>You have been disabled from the system</h1>
+
+<div class="alert alert-danger" style='text-align: center;' role="alert"> <h1>Your account has been disabled from the system.</h1> </div>
+
+
 
